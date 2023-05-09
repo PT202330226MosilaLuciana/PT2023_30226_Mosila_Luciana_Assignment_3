a931949ee699c29453d4869b9d4a7c45a51fc4c0
mosilaluciana <126771141+mosilaluciana@users.noreply.github.com>
2023-05-09 22:25:32 +0300

    commit

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Client {
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+    private int age;
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String address, String email, int age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Client(String name, String address, String email, int age) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.age = age;
+    }
+
+    public int getId() { return id;}
+    public String getName() {return name;}
+    public String getAddress() {return address;}
+    public int getAge() {return age;}
+    public String getEmail() {return email;}
+
+    public void setId(int id) {this.id = id;}
+    public void setName(String name) {this.name = name;}
+    public void setAddress(String address) {this.address = address;}
+    public void setAge(int age) {this.age = age;}
+    public void setEmail(String email) {this.email = email;}
+
+    @Override
+    public String toString() {
+        return "Client [id=" + id + ", name=" + name + ", address=" + address + ", email=" + email + ", age=" + age
+                + "]";
+    }
+
+}
